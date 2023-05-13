6b791ecc4ed09850ec614f59670969bb866c79dd
moustafashahin122 <65954226+moustafashahin122@users.noreply.github.com>
2023-05-13 19:52:00 +0300

    first commit

---
+++ user.py
@@ -0,0 +1,13 @@
+class user:
+    IDs=[]
+    def __init__(self,fName,lName,email,password,mobile):
+        self.fName=fName
+        self.lName=lName
+        self.password=password
+        self.email=email
+        self.mobile=mobile
+        #generate a unique id for each user
+        id=
+        while id in IDs:
+            
+        
